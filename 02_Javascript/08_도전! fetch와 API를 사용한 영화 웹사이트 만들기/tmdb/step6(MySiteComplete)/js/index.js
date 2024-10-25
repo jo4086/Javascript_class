b96392c1e2a53bc42a436ba89809098fc767f45a
jo4086 <ghzktm5892@gmail.com>
2024-10-25 19:24:29 +0900

    메세지

--- 02_Javascript/08_도전! fetch와 API를 사용한 영화 웹사이트 만들기/tmdb/step6(MySiteComplete)/js/index.js
+++ 02_Javascript/08_도전! fetch와 API를 사용한 영화 웹사이트 만들기/tmdb/step6(MySiteComplete)/js/index.js
@@ -20,56 +20,40 @@ const url = 'https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=
 const getPlayingMovies = async (url) => {
    try {
       const response = await fetch(url, options)
-
       const data = await response.json()
-      // console.log('success data:', data)
-
       const results = data.results
-      // console.log(results)
 
-      // for (const result of results) {
-      //    //서버에서 받은 데이터 출력
-      //    console.log(result)
-      //    console.log(result.id)
-      //    console.log(result.title)
-      //    // 혹은 console.log(result['title'])
-      //    console.log(result.poster_path)
-      //    console.log(result.vote_average)
-      // }
+      const container = document.querySelector('main .container')
+      let rowsHtml = '' // 모든 row를 담을 문자열
 
-      //서버에서 받은 데이터 바인딩(웹브라우저 화면에 출력)
-      /*
-          어떻게 해야할까?
-          (방법1) - 속도는 느리나, 쉬운 방법
-          1. 우선 데이터를 바인딩할 요소의 dom을 가져온다.
-          2. 해당 dom에 데이터를 넣어준다.
+      for (let i = 0; i < results.length; i += 4) {
+         // 각 row는 4개의 card를 가짐
+         let rowHtml = '<div class="row">'
 
-          (방법2) - 속도는 이게 더 빠르나, 초보자에겐 어려운 방법
-          1. 바인딩할 dom을 생성한다. 
-          2. 해당 dom에 데이터를 넣어준 후 화면에 나타낸다.
-      */
+         for (let j = 0; j < 4; j++) {
+            const index = i + j
+            if (index >= results.length) break // results 배열을 벗어나면 중단
 
-      const rows = document.querySelectorAll('main .container .row')
+            const movie = results[index]
+            rowHtml += `
+               <div class="col-sm-3 p-3">
+                  <div class="card">
+                     <a href="./detail.html?movie_id=${movie.id}">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top poster" alt="${movie.title}" />
+                     </a>
+                     <div class="card-body">
+                        <p class="card-text title">${movie.title}</p>
+                        <p class="card-text average">${movie.vote_average.toFixed(1)}점</p>
+                     </div>
+                  </div>
+               </div>`
+         }
 
-      //row 요소를 각각 가져온다.
-      rows.forEach((row, rowIdx) => {
-         //rowIdx:0~5까지
-         //console.log(row)
+         rowHtml += '</div>'
+         rowsHtml += rowHtml // 전체 row 문자열에 추가
+      }
 
-         //각 row 요소가 가진 card 요소를 각각 가져온다.
-         const cards = row.querySelectorAll('.card')
-         cards.forEach((card, cardIdx) => {
-            //cardIdx: 0~4까지
-            //console.log(card)
-            const index = cardIdx + rowIdx * 4
-            card.querySelector('.title').innerText = results[index].title
-            card.querySelector('.average').innerText = `${results[index].vote_average.toFixed(1)}점`
-            // w500은 이미지의 width를 500px로 서버에서 가져온다
-            card.querySelector('img').src = `https://image.tmdb.org/t/p/w500${results[index].poster_path}`
-            card.querySelector('img').alt = results[index].title
-            card.querySelector('a').href = `./detail.html?movie_id=${results[index].id}`
-         })
-      })
+      container.innerHTML = rowsHtml // 컨테이너에 모든 row 추가
    } catch (error) {
       console.error('에러 발생:', error)
    }
